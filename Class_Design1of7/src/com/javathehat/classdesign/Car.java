a73ab06aa1ecdd1460c5fa0228ec2442e8da5247
Karol Wójcik <kontakt@javathehat.com>
2016-11-02 06:34:36 +0100

    ClassDesign1 init

---
+++ Class_Design1of7/src/com/javathehat/classdesign/Car.java
@@ -0,0 +1,13 @@
+package com.javathehat.classdesign;
+
+/**
+ * @author kontakt@javathehat.com
+ */
+public class Car {
+	
+	public int speed = 0;
+	protected boolean engineStarted = false;
+	int fuel = 100;
+	private long engineNumnber = 101961412;
+
+}
---
+++ Class_Design1of7/src/com/javathehat/classdesign/Driver.java
@@ -0,0 +1,15 @@
+package com.javathehat.classdesign;
+
+/**
+ * @author kontakt@javathehat.com
+ */
+public class Driver {
+	
+	public static void main(String[] args){
+		Car car = new Car();
+		car.engineStarted = true;
+		car.speed = 120;
+		car.fuel = 50;
+	}
+
+}
